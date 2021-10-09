5e96f4cca029713cc5f1646268e5e3d4c8887e0c
Ahsan Habib Nahid <ahsanhabib3239@gmail.com>
2021-10-09 17:29:49 +0600

    netlify error solve

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import TeamDetails from './components/TeamDetails/TeamDetails';
 
